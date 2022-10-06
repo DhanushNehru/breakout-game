043efe266b23461172b7205f5b2d78ffb4ced5fc
dani0105 <danielrojas0105@gmail.com>
2022-10-06 07:54:56 -0600

    Add block lives

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var leftPressed = false;
 var brickRowCount = 5;
 var brickColumnCount = 8;
 var brickWidth = 100;
-var brickHeight = 10;
+var brickHeight = 15;
 var brickPadding = 7;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
@@ -73,7 +73,7 @@ let bricks = [];
 for (c=0; c<brickColumnCount; c++) {
 	bricks[c] = [];
 	for (r=0; r<brickRowCount; r++) {
-		bricks[c][r] = {x: 0, y:0, status: 1};
+		bricks[c][r] = {x: 0, y:0, status: 1, lives: getRandomArbitrary(1,5,true)};
 	}
 }
 
@@ -103,6 +103,12 @@ function drawBricks() {
 				ctx.rect(brickX, brickY, brickWidth, brickHeight);
 				ctx.fillStyle = utilsColor(c,r, modeColor);
 				ctx.fill();
+
+        // ddraw block lives
+        ctx.font = `${brickHeight-2}px Arial`;
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText(bricks[c][r].lives, brickX+(brickWidth/2), brickY+brickHeight-2);
+
 				ctx.closePath();
 			}
 		}
@@ -132,8 +138,11 @@ function collisionDetection() {
 			if(b.status  == 1) {
 				if(x+ballRadius > b.x && x-ballRadius < b.x+brickWidth && y+ballRadius > b.y && y-ballRadius < b.y+brickHeight) {
 					dy = -dy;
-					b.status = 0;
-					score++;
+          b.lives -= 1;
+          if(b.lives == 0){
+            b.status = 0;
+            score++;
+          }
 					if(score == brickRowCount*brickColumnCount) {
 						resetValues(success);
 					}
@@ -207,8 +216,12 @@ function draw() {
 document.addEventListener("mousemove", mouseMoveHandler);
 
 // get a random value between two values
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomArbitrary(min, max,toInt=false) {
+  let value = Math.random() * (max - min) + min;
+  if(toInt){
+    return Math.round(value)
+  }
+  return value
 }
 
 function mouseMoveHandler(e) {
