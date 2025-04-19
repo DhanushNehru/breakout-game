1683c8b10bb3793bfe3e9bb9e2fa2a2f353bd980
adnantech17 <adnantech17@gmail.com>
2025-04-19 15:08:23 +0600

    fix: refactoring the codebase for maintainability

---
+++ scripts/config.js
@@ -0,0 +1,45 @@
+const config = {
+    // Canvas
+    canvas: document.getElementById('myCanvas'),
+    
+    // Ball
+    ballRadius: 25,
+    initialSpeed: 2,
+    
+    // Paddle
+    paddleHeight: 10,
+    paddleWidth: 100,
+    
+    // Bricks
+    brickRowCount: 5,
+    brickColumnCount: 8,
+    brickWidth: 100,
+    brickHeight: 15,
+    brickPadding: 7,
+    brickOffsetTop: 30,
+    brickOffsetLeft: 30,
+    
+    // Game state
+    initialLives: 3,
+    modeColor: 'row'
+};
+
+const elements = {
+    failure: document.querySelector('.failure'),
+    failureBtn: document.querySelector('#failureBtn'),
+    success: document.querySelector('.success'),
+    successBtn: document.querySelector('#successBtn'),
+    pauseBtn: document.getElementById('pauseBtn'),
+    speedSlider: document.querySelector("#speed-slider"),
+    sizeSlider: document.querySelector("#size-slider")
+};
+
+function getRandomArbitrary(min, max, toInt=false) {
+    let value = Math.random() * (max - min) + min;
+    if(toInt){
+        return Math.round(value)
+    }
+    return value
+}
+
+// If utilsColor is defined in the global space, we don't need to declare it here 
